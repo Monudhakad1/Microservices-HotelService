4fc48a561a32064a14f30eba1c918ce92916a3f2
Monudhakad1 <monudhakad055@gmail.com>
2026-04-09 19:09:48 +0530

    INTILIAZE

---
+++ src/main/java/com/hotelmicroservice/Hoteldetails/HoteldetailsApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmicroservice.Hoteldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteldetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteldetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelmicroservice/Hoteldetails/HoteldetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelmicroservice.Hoteldetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteldetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
